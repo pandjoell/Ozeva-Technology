14d93989beacf97281b6b2a2a0a60d95b2ffef02
TEGARPEPE <tegarputrapratama000@gmail.com>
2025-12-09 08:16:03 +0700

    Improve Ui Sales and improve SEO

--- wp-config.php
+++ wp-config.php
@@ -20,7 +20,7 @@
 
 // ** Database settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
-define( 'DB_NAME', 'ozeva.com' );
+define( 'DB_NAME', 'ozeva_com' );
 
 /** Database username */
 define( 'DB_USER', 'root' );
